3319e473f7e1e3a134e77bff3e2a728d8534aa6e
yona3 <kou.y.0905@icloud.com>
2021-09-03 00:11:59 +0900

    add: http handle

--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-	"github.com/yona3/go-line-bot-sample/api"
+	"github.com/yona3/go-line-bot-sample/linebot"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	loadEnv()
-	api.GetRandomCat()
-	api.GetRandomDog()
+
+	handler, err := linebot.NewHandler()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	handler.HandleEvents(linebot.Webhook)
+	http.Handle("/callback", handler)
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 func loadEnv() {
