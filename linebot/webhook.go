cbf254461792e3aab8228090e2fa35c20a00d86b
yona3 <kou.y.0905@icloud.com>
2021-09-03 00:11:44 +0900

    add: webhook

---
+++ linebot/webhook.go
@@ -0,0 +1,50 @@
+package linebot
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"github.com/yona3/go-line-bot-sample/api"
+)
+
+func Webhook(events []*linebot.Event, r *http.Request) {
+	handler, err := NewHandler()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	bot, err := NewBot(handler)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, e := range events {
+		if e.Type == linebot.EventTypeMessage {
+			switch msg := e.Message.(type) {
+			case *linebot.TextMessage:
+				text := msg.Text
+				if strings.Contains(text, "いぬ") {
+					replyDog := api.GetRandomDog()
+					if _, err := bot.ReplyMessage(e.ReplyToken, linebot.NewImageMessage(replyDog, replyDog)).Do(); err != nil {
+						log.Fatal(err)
+					}
+				} else if strings.Contains(text, "ねこ") {
+					replyCat := api.GetRandomCat()
+					if _, err := bot.ReplyMessage(e.ReplyToken, linebot.NewImageMessage(replyCat, replyCat)).Do(); err != nil {
+						log.Fatal(err)
+					}
+				} else {
+					continue
+				}
+			default:
+				replyText := "「いぬ」or「ねこ」でメッセージを送ってね！"
+				if _, err := bot.ReplyMessage(e.ReplyToken, linebot.NewTextMessage(replyText)).Do(); err != nil {
+					log.Fatal(err)
+				}
+			}
+
+		}
+	}
+}
