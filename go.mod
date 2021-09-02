8e09fee5320ef8ad8263a9e8199c57a01fc9ee91
yona3 <kou.y.0905@icloud.com>
2021-09-03 00:11:29 +0900

    add: line bot

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/yona3/go-line-bot-sample
 go 1.17
 
 require github.com/joho/godotenv v1.3.0
+
+require github.com/line/line-bot-sdk-go/v7 v7.10.0 // indirect
---
+++ linebot/init.go
@@ -0,0 +1,20 @@
+package linebot
+
+import (
+	"os"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"github.com/line/line-bot-sdk-go/v7/linebot/httphandler"
+)
+
+func NewHandler() (*httphandler.WebhookHandler, error) {
+	secret := os.Getenv("LINE_CHANNEL_SECRET")
+	token := os.Getenv("LINE_CHANNEL_ACCESS_TOKEN")
+	handler, err := httphandler.New(secret, token)
+	return handler, err
+}
+
+func NewBot(handler *httphandler.WebhookHandler) (*linebot.Client, error) {
+	bot, err := handler.NewClient()
+	return bot, err
+}
